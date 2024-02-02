906d14422e6aaa4024f2d14873a122a2db4ae56b
Victor <menaflores.victor@gmail.com>
2024-02-02 09:24:48 +0100

    +creacion paquetes

---
+++ src/main/java/daw/Tarea5GVictorMenaFlores.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package daw;
+
+/**
+ *
+ * @author Víctor
+ */
+public class Tarea5GVictorMenaFlores {
+
+    public static void main(String[] args) {
+        
+    }
+}
